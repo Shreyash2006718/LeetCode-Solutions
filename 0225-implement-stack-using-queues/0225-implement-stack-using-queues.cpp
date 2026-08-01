42488cacf6e9992564dbaf58c9abb7320c2078be
Shreyash Singh <gs0711911@gmail.com>
2026-08-01 15:35:54 +0530

    Update 0225-implement-stack-using-queues.cpp

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -32,6 +32,45 @@ public:
     }
 };
 
+
+//  This is using it with two queues:
+
+// class MyStack {
+// public:
+//     queue<int> q1, q2;
+
+//     MyStack() {
+
+//     }
+
+//     void push(int x) {
+//         // Step 1: Insert new element into q2
+//         q2.push(x);
+
+//         // Step 2: Move all elements from q1 to q2
+//         while (!q1.empty()) {
+//             q2.push(q1.front());
+//             q1.pop();
+//         }
+
+//         // Step 3: Swap q1 and q2
+//         swap(q1, q2);
+//     }
+
+//     int pop() {
+//         int val = q1.front();
+//         q1.pop();
+//         return val;
+//     }
+
+//     int top() {
+//         return q1.front();
+//     }
+
+//     bool empty() {
+//         return q1.empty();
+//     }
+// };
 /**
  * Your MyStack object will be instantiated and called as such:
  * MyStack* obj = new MyStack();
