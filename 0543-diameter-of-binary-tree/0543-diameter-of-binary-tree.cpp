d1fda57588460336c9c2af1aa0b06971128d1f99
Shreyash Singh <gs0711911@gmail.com>
2026-09-16 14:47:24 +0530

    Time: 8 ms (17.75%), Space: 26.2 MB (7.9%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -1,24 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
 
-    int diameter = 0;
+    int helperfunction(TreeNode*root){
+        if(root==NULL) return 0;
+        stack<TreeNode*> st;
+        vector<TreeNode*> storage;
+        unordered_map<TreeNode*,int> height;
+        st.push(root);
+        int diameter =0;
+        while(!(st.empty())){
+            TreeNode* node = st.top();
+            st.pop();
+            if(node->left) st.push(node->left);
+            if(node->right) st.push(node->right);
+            storage.push_back(node);
+        }
+        reverse(storage.begin(),storage.end());
 
-    int height(TreeNode* root) {
+        for(TreeNode* node:storage){
 
-        if (root == NULL)
-            return 0;
+            int leftheight =0;
+            int  rightheight =0;
+            if(node->left){
+                leftheight= height[node->left];
+            }
+            if(node->right){
+                rightheight = height[node->right];
+            }
 
-        int leftHeight = height(root->left);
-        int rightHeight = height(root->right);
-        diameter = max(diameter, leftHeight + rightHeight);
+            height[node] = 1+max(leftheight, rightheight);
 
-        return 1+  max(leftHeight, rightHeight);
-    }
-
-    int diameterOfBinaryTree(TreeNode* root) {
-
-        height(root);
+            diameter= max(diameter, leftheight+rightheight);
+        }
 
         return diameter;
+
+    }    
+
+
+    int diameterOfBinaryTree(TreeNode* root) {
+         return helperfunction(root);
+
     }
 };
